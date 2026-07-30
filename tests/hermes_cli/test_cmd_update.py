NousResearch/hermes-agent

fix(config): scope profile migration home override

tests/hermes_cli/test_cmd_update.py
@@ -649,6 +649,74 @@ class TestCmdUpdateMigrationPrompt:
             assert "display.new_widget" in out
 
 
+class TestCmdUpdateProfileConfigMigration:
+    """Profile migrations remain scoped to the profile being updated."""
+
+    def test_migration_uses_target_scope_and_restores_outer_override(self, tmp_path):
+        from hermes_constants import (
+            get_hermes_home,
+            reset_hermes_home_override,
+            set_hermes_home_override,
+        )
+        from hermes_cli.main import _migrate_profile_config
+
+        profile = SimpleNamespace(name="work", path=tmp_path / "profiles" / "work")
+        outer_home = tmp_path / "outer"
+        outer_token = set_hermes_home_override(outer_home)
+        homes_seen_during_migration = []
+
+        try:
+            with (
+                patch("hermes_cli.config.check_config_version", return_value=(1, 2)),
+                patch("hermes_cli.config.get_missing_env_vars", return_value=[]),
+                patch("hermes_cli.config.get_missing_config_fields", return_value=[]),
+                patch(
+                    "hermes_cli.config.migrate_config",
+                    side_effect=lambda **_: homes_seen_during_migration.append(
+                        get_hermes_home()
+                    ),
+                ),
+            ):
+                _migrate_profile_config(profile)
+
+            assert homes_seen_during_migration == [profile.path]
+            assert get_hermes_home() == outer_home
+        finally:
+            reset_hermes_home_override(outer_token)
+
+    def test_migration_failure_restores_outer_override(self, tmp_path):
+        from hermes_constants import (
+            get_hermes_home,
+            reset_hermes_home_override,
+            set_hermes_home_override,
+        )
+        from hermes_cli.main import _migrate_profile_config
+
+        profile = SimpleNamespace(name="work", path=tmp_path / "profiles" / "work")
+        outer_home = tmp_path / "outer"
+        outer_token = set_hermes_home_override(outer_home)
+        homes_seen_during_migration = []
+
+        def failing_migration(**_):
+            homes_seen_during_migration.append(get_hermes_home())
+            raise RuntimeError("migration failed")
+
+        try:
+            with (
+                patch("hermes_cli.config.check_config_version", return_value=(1, 2)),
+                patch("hermes_cli.config.get_missing_env_vars", return_value=[]),
+                patch("hermes_cli.config.get_missing_config_fields", return_value=[]),
+                patch("hermes_cli.config.migrate_config", side_effect=failing_migration),
+                pytest.raises(RuntimeError, match="migration failed"),
+            ):
+                _migrate_profile_config(profile)
+
+            assert homes_seen_during_migration == [profile.path]
+            assert get_hermes_home() == outer_home
+        finally:
+            reset_hermes_home_override(outer_token)
+
+
 class TestCmdUpdateProfileSkillSync:
     """cmd_update syncs bundled skills to all profiles, including the active one.
 

bottombgr-design/hermes

tests/hermes_cli/test_cmd_update.py
@@ -346,6 +346,74 @@ class TestCmdUpdateMigrationPrompt:
             assert "display.new_widget" in out
 
 
+class TestCmdUpdateProfileConfigMigration:
+    """Profile migrations remain scoped to the profile being updated."""
+
+    def test_migration_uses_target_scope_and_restores_outer_override(self, tmp_path):
+        from hermes_constants import (
+            get_hermes_home,
+            reset_hermes_home_override,
+            set_hermes_home_override,
+        )
+        from hermes_cli.update_cmd import _migrate_profile_config
+
+        profile = SimpleNamespace(name="work", path=tmp_path / "profiles" / "work")
+        outer_home = tmp_path / "outer"
+        outer_token = set_hermes_home_override(outer_home)
+        homes_seen_during_migration = []
+
+        try:
+            with (
+                patch("hermes_cli.config.check_config_version", return_value=(1, 2)),
+                patch("hermes_cli.config.get_missing_env_vars", return_value=[]),
+                patch("hermes_cli.config.get_missing_config_fields", return_value=[]),
+                patch(
+                    "hermes_cli.config.migrate_config",
+                    side_effect=lambda **_: homes_seen_during_migration.append(
+                        get_hermes_home()
+                    ),
+                ),
+            ):
+                _migrate_profile_config(profile)
+
+            assert homes_seen_during_migration == [profile.path]
+            assert get_hermes_home() == outer_home
+        finally:
+            reset_hermes_home_override(outer_token)
+
+    def test_migration_failure_restores_outer_override(self, tmp_path):
+        from hermes_constants import (
+            get_hermes_home,
+            reset_hermes_home_override,
+            set_hermes_home_override,
+        )
+        from hermes_cli.update_cmd import _migrate_profile_config
+
+        profile = SimpleNamespace(name="work", path=tmp_path / "profiles" / "work")
+        outer_home = tmp_path / "outer"
+        outer_token = set_hermes_home_override(outer_home)
+        homes_seen_during_migration = []
+
+        def failing_migration(**_):
+            homes_seen_during_migration.append(get_hermes_home())
+            raise RuntimeError("migration failed")
+
+        try:
+            with (
+                patch("hermes_cli.config.check_config_version", return_value=(1, 2)),
+                patch("hermes_cli.config.get_missing_env_vars", return_value=[]),
+                patch("hermes_cli.config.get_missing_config_fields", return_value=[]),
+                patch("hermes_cli.config.migrate_config", side_effect=failing_migration),
+                pytest.raises(RuntimeError, match="migration failed"),
+            ):
+                _migrate_profile_config(profile)
+
+            assert homes_seen_during_migration == [profile.path]
+            assert get_hermes_home() == outer_home
+        finally:
+            reset_hermes_home_override(outer_token)
+
+
 class TestCmdUpdateProfileSkillSync:
     """cmd_update syncs bundled skills to all profiles, including the active one.
 